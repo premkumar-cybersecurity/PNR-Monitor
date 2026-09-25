606fbd24c0b2a0abf34dd153105dda0cc8dbc7a5
prem-cybersecurity <prem.cybersecurity@gmail.com>
2026-09-25 23:49:41 +0530

    Fix Vercel Express detection

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
-// Vercel entry point. The application itself remains in server/server.js.
-// Exporting the Express app lets Vercel run it as an Express backend.
-module.exports = require("./server/server");
+const express = require("express");
+
+const app = require("./server/server");
+
+module.exports = app;
